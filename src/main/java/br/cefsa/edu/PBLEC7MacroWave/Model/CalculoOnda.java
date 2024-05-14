1da88e0fedb7d80d2e6a3330393a440315b60f7d
lorenzomessias <lorenzo.messias2@gmail.com>
2024-05-13 21:35:14 -0300

    Calculos

--- src/main/java/br/cefsa/edu/PBLEC7MacroWave/Model/CalculoOnda.java
+++ src/main/java/br/cefsa/edu/PBLEC7MacroWave/Model/CalculoOnda.java
@@ -1,48 +1,29 @@
 package br.cefsa.edu.PBLEC7MacroWave.Model;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CalculoOnda {
 
-    private static final double duracao = 1;
-    private static final double amplitude = 1;
+    private static final int harmonicas = 100;
+    private static final double amplitudeMaxima = 1;
 
-    public static Map<Double, Double> calcularOndaSenoidal(double amplitude, double frequencia, double fase, double duracao, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
+    public static Map<Double, Double> calcularOndaSenoidal(double amplitudeMaxima, double frequencia, double fase, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
         double periodo = 1 / frequencia;
-        Map<Double, Double> mapaAmplitudes = new HashMap<>();
+        double duracao = periodo * harmonicas;
+        Map<Double, Double> mapaAmplitudes = new LinkedHashMap<>();
         double t = 0;
         while (t < duracao) {
-            double amplitude_final = amplitude * Math.sin(2 * Math.PI * frequencia * t + fase);
+            double amplitude = amplitudeMaxima * Math.abs(Math.sin(2 * Math.PI * frequencia * t + fase));
 
             // Filtragem de acordo com o tipo de canal
             if (canal == Canal.PASSABAIXAS && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
+                mapaAmplitudes.put(t * 1000, amplitude);
             } else if (canal == Canal.PASSAFAIXAS && frequencia >= frequenciaCorteInf && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
+                mapaAmplitudes.put(t * 1000, amplitude);
             } else {
-                mapaAmplitudes.put(t, 0.0); // Frequência não permitida pelo canal
-            }
-
-            t += periodo * 1000000; // Dividindo o período em pequenos incrementos para obter uma representação suave da onda
-        }
-        return mapaAmplitudes;
-    }
-
-    public static Map<Double, Double> calcularOndaQuadrada(double amplitude, double frequencia, double duracao, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
-        double periodo = 1 / frequencia;
-        Map<Double, Double> mapaAmplitudes = new HashMap<>();
-        double t = 0;
-        while (t < duracao) {
-            double amplitude_final = (t % periodo < periodo / 2) ? amplitude : -amplitude;
-
-            // Filtragem de acordo com o tipo de canal
-            if (canal == Canal.PASSABAIXAS && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
-            } else if (canal == Canal.PASSAFAIXAS && frequencia >= frequenciaCorteInf && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
-            } else {
-                mapaAmplitudes.put(t, 0.0); // Frequência não permitida pelo canal
+                mapaAmplitudes.put(t * 1000, 0.0); // Frequência não permitida pelo canal
             }
 
             t += periodo / 100; // Dividindo o período em pequenos incrementos para obter uma representação suave da onda
@@ -50,25 +31,21 @@ public class CalculoOnda {
         return mapaAmplitudes;
     }
 
-    public static Map<Double, Double> calcularOndaTriangular(double amplitude, double frequencia, double duracao, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
+    public static Map<Double, Double> calcularOndaQuadrada(double amplitudeMaxima, double frequencia, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
         double periodo = 1 / frequencia;
-        Map<Double, Double> mapaAmplitudes = new HashMap<>();
+        double duracao = periodo * harmonicas;
+        Map<Double, Double> mapaAmplitudes = new LinkedHashMap<>();
         double t = 0;
         while (t < duracao) {
-            double amplitude_final;
-            if (t % periodo < periodo / 2) {
-                amplitude_final = (4 * amplitude / periodo) * t;
-            } else {
-                amplitude_final = (-4 * amplitude / periodo) * t + 4 * amplitude;
-            }
+            double amplitude = (t % periodo < periodo / 2) ? amplitudeMaxima : -amplitudeMaxima;
 
             // Filtragem de acordo com o tipo de canal
             if (canal == Canal.PASSABAIXAS && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
+                mapaAmplitudes.put(t * 1000, amplitude);
             } else if (canal == Canal.PASSAFAIXAS && frequencia >= frequenciaCorteInf && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
+                mapaAmplitudes.put(t * 1000, amplitude);
             } else {
-                mapaAmplitudes.put(t, 0.0); // Frequência não permitida pelo canal
+                mapaAmplitudes.put(t * 1000, 0.0); // Frequência não permitida pelo canal
             }
 
             t += periodo / 100; // Dividindo o período em pequenos incrementos para obter uma representação suave da onda
@@ -76,20 +53,42 @@ public class CalculoOnda {
         return mapaAmplitudes;
     }
 
-    public static Map<Double, Double> calcularOndaDenteDeSerra(double amplitude, double frequencia, double duracao, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
+    public static Map<Double, Double> calcularOndaTriangular(double amplitudeMaxima, double frequencia, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
         double periodo = 1 / frequencia;
-        Map<Double, Double> mapaAmplitudes = new HashMap<>();
+        double duracao = periodo * harmonicas;
+        Map<Double, Double> mapaAmplitudes = new LinkedHashMap<>();
         double t = 0;
         while (t < duracao) {
-            double amplitude_final = (amplitude * t) % periodo;
+            double amplitude = amplitudeMaxima - 2 * amplitudeMaxima * (Math.abs(t % periodo - periodo / 2) / (periodo / 2));
+            // Filtragem de acordo com o tipo de canal
+            if (canal == Canal.PASSABAIXAS && frequencia <= frequenciaCorteSup) {
+                mapaAmplitudes.put(t * 1000, amplitude);
+            } else if (canal == Canal.PASSAFAIXAS && frequencia >= frequenciaCorteInf && frequencia <= frequenciaCorteSup) {
+                mapaAmplitudes.put(t * 1000, amplitude);
+            } else {
+                mapaAmplitudes.put(t * 1000, 0.0); // Frequência não permitida pelo canal
+            }
+
+            t += periodo / 100; // Dividindo o período em pequenos incrementos para obter uma representação suave da onda
+        }
+        return mapaAmplitudes;
+    }
+
+    public static Map<Double, Double> calcularOndaDenteDeSerra(double amplitudeMaxima, double frequencia, Canal canal, double frequenciaCorteSup, double frequenciaCorteInf) {
+        double periodo = 1 / frequencia;
+        double duracao = periodo * harmonicas;
+        Map<Double, Double> mapaAmplitudes = new LinkedHashMap<>();
+        double t = 0;
+        while (t < duracao) {
+            double amplitude = (amplitudeMaxima * t) % periodo;
 
             // Filtragem de acordo com o tipo de canal
             if (canal == Canal.PASSABAIXAS && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
+                mapaAmplitudes.put(t * 1000, amplitude);
             } else if (canal == Canal.PASSAFAIXAS && frequencia >= frequenciaCorteInf && frequencia <= frequenciaCorteSup) {
-                mapaAmplitudes.put(t, amplitude_final);
+                mapaAmplitudes.put(t * 1000, amplitude);
             } else {
-                mapaAmplitudes.put(t, 0.0); // Frequência não permitida pelo canal
+                mapaAmplitudes.put(t * 1000, 0.0); // Frequência não permitida pelo canal
             }
 
             t += periodo / 100; // Dividindo o período em pequenos incrementos para obter uma representação suave da onda
@@ -102,11 +101,11 @@ public class CalculoOnda {
         frequenciaCorteInf = frequenciaCorteInf * 1000; // Convertendo kHz para Hz
         frequenciaCorteSup = frequenciaCorteSup * 1000; // Convertendo kHz para Hz
         return switch (categoria) {
-            case SENOIDAL -> calcularOndaSenoidal(amplitude, frequencia, 0, duracao, canal, frequenciaCorteSup, frequenciaCorteInf);
-            case QUADRADA -> calcularOndaQuadrada(amplitude, frequencia, duracao, canal, frequenciaCorteSup, frequenciaCorteInf);
-            case TRIANGULAR -> calcularOndaTriangular(amplitude, frequencia, duracao, canal, frequenciaCorteSup, frequenciaCorteInf);
-            case DENTEDESERRA -> calcularOndaDenteDeSerra(amplitude, frequencia, duracao, canal, frequenciaCorteSup, frequenciaCorteInf);
-            default -> new HashMap<>();
+            case SENOIDAL -> calcularOndaSenoidal(amplitudeMaxima, frequencia, 0, canal, frequenciaCorteSup, frequenciaCorteInf);
+            case QUADRADA -> calcularOndaQuadrada(amplitudeMaxima, frequencia, canal, frequenciaCorteSup, frequenciaCorteInf);
+            case TRIANGULAR -> calcularOndaTriangular(amplitudeMaxima, frequencia, canal, frequenciaCorteSup, frequenciaCorteInf);
+            case DENTEDESERRA -> calcularOndaDenteDeSerra(amplitudeMaxima, frequencia, canal, frequenciaCorteSup, frequenciaCorteInf);
+            default -> new LinkedHashMap<>();
         };
     }
 }
